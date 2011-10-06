4dc6c0c7a4793e2c0e2878fc86b4c5bfd02babf1
chocopuyo <chocopuyo@gmail.com>
2011-10-06 21:28:26 +0900

    最初のコミットです。システムは何も出来ていません。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicMailing::Application.config.secret_token = 'f2f94694c4239f204993f0b6c7bc0ea4f3900e4c1c55a090bf5ee5246b3474bbf0c6c10b24a5b0270bd4052e9cf16b7155972f7ccabbc4732b808f67d109fd15'
