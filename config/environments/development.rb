a6d31633adddd15c073b9499c2af58414a5a0500
chocopuyo <chocopuyo@gmail.com>
2011-10-07 01:37:50 +0900

    bundle installした余計なファイルを消しました...ひょっとしたらあったほうがいいかも..?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ BasicMailing::Application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'gmail.com',
-    :user_name            => '',
+    :user_name            => '@gmail.com',
     :password             => '',
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
